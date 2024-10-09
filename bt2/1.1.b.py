e044b6dc8607f2b00e6cd6046855915c6e7e2ad3
quanmcvn <quanninhduong1@gmail.com>
2024-10-09 16:29:41 +0700

    Second week's hw

---
+++ bt2/1.1.b.py
@@ -0,0 +1,21 @@
+from math import pi, sin
+import matplotlib.pyplot as plt
+
+Ts= 0.0001  # % khoảng thời gian lấy mẫu (s)
+
+t = range(0, int(0.5 / Ts), 1) #0:Ts:0.5; #% thời gian mô phỏng (s)
+real_t = [t_ * Ts for t_ in t]
+A = 1;        # % biên độ
+
+f = 1000;        #% tần số (Hz)
+
+phi = 0;    #   % pha
+
+s = [A *sin(2*pi*f*t_ * Ts + phi) for t_ in t]
+
+plt.title('Sine wave 1kHz')
+plt.plot(real_t, s)
+plt.xlabel('t')
+plt.ylabel('s')
+
+plt.show()
---
+++ bt2/1.2.a.py
@@ -0,0 +1,25 @@
+from math import pi, sin
+import matplotlib.pyplot as plt
+
+Ts= 0.0001  # % khoảng thời gian lấy mẫu (s)
+
+t = range(0, int(0.5 / Ts), 1) #0:Ts:0.5; #% thời gian mô phỏng (s)
+real_t = [t_ * Ts for t_ in t]
+
+def A(t):
+	if 0.2 <= t <= 0.3:
+		return 1
+	return 0
+
+f = 100;        #% tần số (Hz)
+
+phi = 0;    #   % pha
+
+s = [A(t_ * Ts) for t_ in t]
+
+plt.title('A(t)')
+plt.plot(real_t, s)
+plt.xlabel('t')
+plt.ylabel('s')
+
+plt.show()
